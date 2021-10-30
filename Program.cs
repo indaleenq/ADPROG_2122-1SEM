7d3e44c6fa4027e2cb3491344b50326caa7f2bf4
indaleen <inquinsayas@gmail.com>
2021-10-30 20:39:00 +0800

    added initial csharp demo
    console writeline, readline and variables

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, please type your name: ");
+            //string name; // variable declaration
+            string name = Console.ReadLine(); // variable initialization
+            Console.WriteLine($"Welcome {name}");
+
+            Console.Write("Now, please enter your age: ");
+            //string age = Console.ReadLine();
+            //int ageNumber = Convert.ToInt32(age);
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            var newAge = age - 2;
+            Console.WriteLine($"your new age is {newAge}");
+        }
+    }
+}
